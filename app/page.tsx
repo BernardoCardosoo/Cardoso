5d999a8539c85ee2fc92f9bb7e2d3d2a999aecbf
BernardoCardosoo <146895372+BernardoCardosoo@users.noreply.github.com>
2023-12-06 00:09:17 +0000

    fix: lib

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+
+
+export const revalidate = 0
+export default function Home() {
+  return (
+    <div className="bg-[#3d3d3d]">
+      <div className="max-w-[1440px] m-auto ">
+        <Header />
+       
+      </div>
+    </div>
+
+
+  )
+}
